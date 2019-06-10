d65fb045e6519b338756ad638b7672f3e8721adc
unknown <manateeman24@hotmail.com>
2019-06-10 16:25:59 -0600

    some more changes

--- src/components/addContactForm.vue
+++ src/components/addContactForm.vue
@@ -2,12 +2,32 @@
   <!-- action -->
   <form class="add-contact-form" @submit="_onSubmit">
     <h2>Create Contact</h2>
-    <!-- <form @submit="_validate" action='apiURL' metho
-    d='post'> -->
     <div>
-      <label>Label</label>
-      <input/>
-      <div>
+      <label>Name</label>
+      <input type="text" v-model="name" placeholder="John Smith" v-on:blue="_isalphaNum" required>
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="phone" v-model="phone" placeholder="xxx-xxx-xxxx">
+    </div>
+    <div>
+      <label>Email</label>
+      <input type="email" v-model="email" placeholder="joe@email.com">
+    </div>
+    <div>
+      <label>Add to Favorites?</label>
+      <input type="checkbox" v-model="isFavorite">
+    </div>
+    <div v-if="errors.length">
+      <h5>Please fix the following Errors:</h5>
+      <ul>
+        <li v-for="error in errors" :key="error">{{error}}</li>
+      </ul>
+    </div>
+    <div v-if="succeeded">
+      <p class='success-alert'>Contact Created!</p>
+    </div>
+
     <button type="submit">Submit</button>
   </form>
 </template>
@@ -22,50 +42,43 @@ export default {
       email: "",
       phone: "",
       isFavorite: false,
-      fields: [
-        {
-          name: "name",
-          label: "Name",
-          type: "text",
-          placeHolder: "John Smith"
-        },
-        {
-          name: "email",
-          label: "Email",
-          type: "email",
-          placeHolder: "joe@email.com"
-        },
-        {
-          name: "phone",
-          label: "Phone Number",
-          type: "phone",
-          placeHolder: "xxx-xxx-xxxx"
-        },
-        { name: "is_favorite", label: "Add to Favorites?", type: "checkbox" }
-      ]
+      errors: [],
+      succeeded: true
+
     };
   },
 
   methods: {
     async _onSubmit(e) {
-      console.log('here')
-      console.log(this.name, this.email, this.phone, this.isFavorite);
-      // /[a-z]/.test(this.name)
-      // /[0-9]/.test(this.phone)
-      // /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this.email)
+      this.errors = [];
+
+      const emailregex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
       const data = {
         name: this.name,
         email: this.email,
         phone: this.phone,
         is_favorite: this.isFavorite
       };
+
+      if (!/[A-Za-z]/.test(this.name))
+        this.errors.push("You must enter a valid name");
+
+      if (!/[0-9]/.test(this.phone))
+        this.errors.push("You must enter a valid phone number");
+
+      if (!emailregex.test(this.email))
+        this.errors.push("You must enter a valid email");
+
       try {
         // const res = await ContactService.createContact(data)
+        this.succeeded = true
+        return res.data;
       } catch (error) {
         alert("Unable to create contact");
         console.error(error);
       }
-      e.preventDefault()
+      e.preventDefault();
     }
   }
 };
@@ -74,13 +87,20 @@ export default {
 <style scoped lang="sass">
 .add-contact-form
   height: 40%
+  width: 50vw
+  margin: auto
   display: flex
   align-items: center
   justify-content: space-around
   flex-direction: column
+  background-color: #eaf1f8
   button
     width: 100px
   input
     margin-bottom: 20px
     margin-left: 20px
+  li
+    list-style: none
+    background-color: #f2d896
+    margin-top: 10px
 </style>
--- src/services/ContactService.js
+++ src/services/ContactService.js
@@ -2,5 +2,5 @@ import api from '@/api'
 
 export default {
   getContacts: () => api().get(`/contacts`),
-  createContact: data => api().post('/contact', data)
+  createContact: data => api().post('/contact', JSON.stringify(data))
 }
